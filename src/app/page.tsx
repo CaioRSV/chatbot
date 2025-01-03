69324628c95d0b399196c1926071220f6af720dd
Caio Verçosa <31866855+CaioRSV@users.noreply.github.com>
2025-01-03 10:13:16 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center text-white">
+      <p>teste</p>
+    </div>
+  );
+}
